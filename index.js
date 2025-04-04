fbc204041364118cae344f9a1de66c33e12fd172
Danil Mironenko <danilmironenko41@gmail.com>
2025-04-04 21:14:35 +0300

    soem cahnges

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ bot.on("message", async (msg) => {
   }
 });
 
-app.post("/web-data", async (req, res) => {
+app.post("/data", async (req, res) => {
   const { queryId, products, totalPrice } = req.body;
   try {
     await bot.answerWebAppQuery(queryId, {
